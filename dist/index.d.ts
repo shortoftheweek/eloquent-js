11e2d4557a663c4c593a4b4266e6e66411a71ff3
Matt Kenefick <keneficksays@gmail.com>
2021-04-27 16:08:29 -0400

    Export interfaces

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,4 +2,5 @@ import ActiveRecord from './ActiveRecord';
 import Collection from './Collection';
 import Model from './Model';
 import Request from './Http/Request';
-export { ActiveRecord, Collection, Model, Request };
+import { IAttributes, ICachedResponse, ICachedResponses, ICollectionMeta, IDispatcher, IModelRequestOptions, IModelRequestQueryParams, IPagination, ISortOptions } from './Interfaces';
+export { ActiveRecord, Collection, IAttributes, ICachedResponse, ICachedResponses, ICollectionMeta, IDispatcher, IModelRequestOptions, IModelRequestQueryParams, IPagination, ISortOptions, Model, Request, };
--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,5 @@ import ActiveRecord from './ActiveRecord';
 import Collection from './Collection';
 import Model from './Model';
 import Request from './Http/Request';
-export { ActiveRecord, Collection, Model, Request };
+export { ActiveRecord, Collection, Model, Request, };
 //# sourceMappingURL=index.js.map
