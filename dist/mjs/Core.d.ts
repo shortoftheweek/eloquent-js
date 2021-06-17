549caf23ed9025a92d7d0b06d339011191b1e11d
Matt Kenefick <keneficksays@gmail.com>
2021-06-17 12:19:14 -0400

    V2

--- dist/mjs/Core.d.ts
+++
@@ -1,9 +0,0 @@
-import { Dispatcher } from 'dispatcher';
-import { IDispatcher } from './Interfaces';
-export default class Core extends Dispatcher implements IDispatcher {
-    constructor(options?: object);
-    trigger(eventName: string, data?: any): void;
-    dispatch(eventName: string, data?: any): void;
-    on(eventName: string, callback: (data?: any) => void): void;
-    off(eventName: string, callback?: any): void;
-}
--- dist/mjs/index.d.ts
+++
@@ -1,7 +0,0 @@
-import ActiveRecord from './ActiveRecord';
-import Core from './Core';
-import Collection from './Collection';
-import Model from './Model';
-import Request from './Http/Request';
-import { IAttributes, ICachedResponse, ICachedResponses, ICollectionMeta, IDispatcher, IModelRequestOptions, IModelRequestQueryParams, IPagination, ISortOptions } from './Interfaces';
-export { ActiveRecord, Core, Collection, IAttributes, ICachedResponse, ICachedResponses, ICollectionMeta, IDispatcher, IModelRequestOptions, IModelRequestQueryParams, IPagination, ISortOptions, Model, Request, };
